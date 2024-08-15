4de60d40a4bf7ceabe210d627c060a62fd6de3ba
Wanderbagger <ronikut1988@mail.ru>
2024-08-15 13:13:45 +0300

    Initial commit

---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,14 @@
+class Tavern {
+}
+
+fun main(args: Array<String>) {
+
+    var beverage = readLine()
+ beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
+    println(beverage)
+}
