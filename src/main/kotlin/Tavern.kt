ce97c586a807a32cfce88181b030bbe809dd8801
Wanderbagger <ronikut1988@mail.ru>
2024-08-16 16:45:07 +0300

    Продолжение учебы - стр. 162

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,16 +1,47 @@
 class Tavern {
 }
 const val TAVERN_NAME = "Taernyl's Folly"
-
+var playerGold = 10
+var playerSilver = 10
 fun main(args: Array<String>) {
-placeOrder()
-
-
-
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-private fun placeOrder(){
+fun performPurchase(price : Double){
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver/100.0)
+    println("Total purse: $totalPurse")
+    print("Purchasing item for $price")
+    val remainungBalance = totalPurse - price
+}
+private fun displayBalance(){
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
+}
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value){
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|_|"
+            else -> it.value
+        }
+    }
+
+private fun placeOrder(menuData: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+    performPurchase(price.toDouble())
+    val phrase = if(name == "Dragon's Breath") {
+        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}"
+    } else {
+        "Madrigal says: Thanks for the $name"
+    }
+    println(phrase)
+
 }
