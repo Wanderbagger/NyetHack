9894fe59d466ea4aa667f0732697081771f98762
Wanderbagger <ronikut1988@mail.ru>
2024-08-19 13:58:48 +0300

    Продолжение учебы - стр. 179

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,18 +1,27 @@
+import kotlin.math.roundToInt
 class Tavern {
 }
 const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
+val patronList: List<String> = listOf("Eli", "Mordoc", "Sophie")
 fun main(args: Array<String>) {
     placeOrder("shandy,Dragon's Breath,5.91")
+    println(patronList)
 }
 
 fun performPurchase(price : Double){
     displayBalance()
     val totalPurse = playerGold + (playerSilver/100.0)
     println("Total purse: $totalPurse")
-    print("Purchasing item for $price")
-    val remainungBalance = totalPurse - price
+    println("Purchasing item for $price")
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+    val remainingGold = remainingBalance.toInt()
+    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSilver
+    displayBalance()
 }
 private fun displayBalance(){
     println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
@@ -34,14 +43,19 @@ private fun placeOrder(menuData: String){
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
     val (type, name, price) = menuData.split(',')
-    val message = "Madrigal buys a $name ($type) for $price"
-    println(message)
-    performPurchase(price.toDouble())
-    val phrase = if(name == "Dragon's Breath") {
-        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}"
-    } else {
-        "Madrigal says: Thanks for the $name"
-    }
-    println(phrase)
+    val totalPurse = playerGold + (playerSilver/100.0)
 
+    if(totalPurse > price.toDouble()) {
+        val message = "Madrigal buys a $name ($type) for $price"
+        println(message)
+        performPurchase(price.toDouble())
+        val phrase = if (name == "Dragon's Breath") {
+            "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}"
+        } else {
+            "Madrigal says: Thanks for the $name"
+        }
+        println(phrase)
+    } else {
+        print("Taernyl says: You've goy not enough money, milord!")
+    }
 }
