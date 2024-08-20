0f30b6a75d5724d7545a1bcfc54871a11b472572
Wanderbagger <ronikut1988@mail.ru>
2024-08-20 14:04:43 +0300

    Продолжение учебы - стр. 203

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,15 +1,34 @@
 import kotlin.math.roundToInt
+import java.io.File
 class Tavern {
 }
 const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsvorth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-items.txt")
+    .readText()
+    .split("\n")
 fun main(args: Array<String>) {
-    placeOrder("shandy,Dragon's Breath,5.91")
-    patronList.remove("Eli")
-    patronList.add("Alex")
-    println(patronList)
+    /*
+    (0..9).forEach{
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val  name = "$first $last"
+        uniquePatrons += name
+    }
+    println(uniquePatrons)
+
+    var orderCount = 0
+    while (orderCount <= 9){
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
+    }
+
+     */
+    printMenu()
 }
 
 fun performPurchase(price : Double){
@@ -40,24 +59,44 @@ private fun toDragonSpeak(phrase: String) =
         }
     }
 
-private fun placeOrder(menuData: String){
+private fun placeOrder(patronName: String, menuData: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speaks with $tavernMaster about their order.")
+    println("$patronName speaks with $tavernMaster about their order.")
     val (type, name, price) = menuData.split(',')
     val totalPurse = playerGold + (playerSilver/100.0)
 
     if(totalPurse > price.toDouble()) {
-        val message = "Madrigal buys a $name ($type) for $price"
+        val message = "$patronName buys a $name ($type) for $price"
         println(message)
-        performPurchase(price.toDouble())
+        // performPurchase(price.toDouble())
         val phrase = if (name == "Dragon's Breath") {
-            "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}"
+            "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}!"
         } else {
-            "Madrigal says: Thanks for the $name"
+            "$patronName says: Thanks for the $name"
         }
         println(phrase)
     } else {
         print("Taernyl says: You've goy not enough money, milord!")
     }
+}
+
+private fun printMenu(){
+    val firstLine = "*** Welcome to Taernyl's Folly ***"
+    println(firstLine)
+    println(firstLine.length)
+
+    for(menuItem in menuList){
+        val (type, name, price) = menuItem.split(',')
+        var dots = "."
+        while (firstLine.length > (name.length + price.length + dots.length)){
+            dots +=  dots
+        }
+        println( firstLine.length)
+        println(name.length + price.length + dots.length)
+    println(name + dots + price)
+    }
+
+
+
 }
