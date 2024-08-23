56def667b9e601b7157e687312e120cf01380804
Wanderbagger <ronikut1988@mail.ru>
2024-08-23 14:09:13 +0300

    Продолжение учебы - стр. 235

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,62 +4,20 @@ class Game {
 }
 
 fun main(args: Array<String>) {
-    val name = "Madrigal"
-    var healthPoints = 89
-    var isBlessed = true
-    val isImmortal = true
-    var glass = 0
+   val player = Player()
+    player.castFireball()
+    val auraColor = player.auraColor()
+    printPlayerStatus(player, auraColor)
 
-    // Aura
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
-
-     // Status
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-
-    castFireball(Random.nextInt(0,100))
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> " is in excellent condition"
-        in 90..99 -> " has few scratches"
-        in 75..89 -> if (isBlessed) {
-            " has some minor wounds, bit is healing quickly!"
-        } else {
-            " has some minor wounds."
-        }
-        in 15..74 -> " looks pretty hurt"
-        else -> " is in awful condition"
-    }
 
-
-
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+private fun printPlayerStatus(player: Player, auraColor: String){
+    println("(Aura: ${player.auraColor()}) " +
+            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
 
-private fun castFireball(numFireballs: Int): Int {
-
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
-    println("Your status is. (x$)")
-    if(numFireballs > 50){
-        return 50
-    } else {
-        return numFireballs
-    }
-}
 
 
