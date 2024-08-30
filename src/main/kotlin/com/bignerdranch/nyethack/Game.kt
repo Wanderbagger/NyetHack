730b34eade96ce8bd85d27c2e9dc46046c4825a6
Wanderbagger <ronikut1988@mail.ru>
2024-08-30 13:56:19 +0300

    Продолжение учебы - стр. 276

--- src/main/kotlin/com/bignerdranch/nyethack/Game.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Game.kt
@@ -6,13 +6,16 @@ class Game {
 fun main(args: Array<String>) {
     val player = Player("Madrigal")
     player.castFireball()
-    val auraColor = player.auraColor()
-    printPlayerStatus(player, auraColor)
+    var currentRoom = TownSquare()
+    println(currentRoom.description())
+    println(currentRoom.load())
+
+    printPlayerStatus(player)
 
 }
 
 
-private fun printPlayerStatus(player: Player, auraColor: String){
+private fun printPlayerStatus(player: Player){
     println("(Aura: ${player.auraColor()}) " +
             "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
     println("${player.name} ${player.formatHealthStatus()}")
