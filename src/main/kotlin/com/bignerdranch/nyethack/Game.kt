a3c1d7e8a48ede5c35832d637a7af9878595ed7b
Wanderbagger <ronikut1988@mail.ru>
2024-09-13 13:52:24 +0300

    Продолжение учебы - стр. 293

--- src/main/kotlin/com/bignerdranch/nyethack/Game.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Game.kt
@@ -15,7 +15,11 @@ object Game {
         while (true) {
             println(currentRoom.description())
             println(currentRoom.load())
+
+            // Состояние игрока
             printPlayerStatus(player)
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
         }
     }
 
@@ -23,6 +27,16 @@ object Game {
         println("(Aura: ${player.auraColor()}) " +
                 "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
         println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+        fun processCommand() = when(command.toLowerCase()){
+            else -> commandNotFound()
+        }
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
     }
 }
 
