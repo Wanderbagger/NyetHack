6554edcb6f4ba4eab8e8e0165f85faa73b9f7ae7
Wanderbagger <ronikut1988@mail.ru>
2024-09-16 14:08:34 +0300

    Продолжение учебы - стр. 311

--- src/main/kotlin/com/bignerdranch/nyethack/Game.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Game.kt
@@ -1,16 +1,37 @@
 package com.bignerdranch.nyethack
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
     Game.play()
 }
 
 object Game {
     val player = Player("Madrigal")
-    var currentRoom = TownSquare()
+    private var currentRoom: Room = TownSquare()
+    private var worldMap = listOf(
+        listOf(currentRoom, Room("Tavern"), Room("Back Room"),
+        Room("Long Corridor"), Room("Generic Room")))
     init {
         println("Welcome, adventurer.")
         player.castFireball()
     }
+
+    private fun move(directionInput: String) =
+        try {
+            val direction = Direction.valueOf(directionInput.toUpperCase())
+            val newPosition = direction.updateCoordinate(player.currentPosition)
+            if (!newPosition.isInBounds) {
+                throw IllegalStateException("$direction is out of bounds.")
+            }
+            val newRoom = worldMap[newPosition.y][newPosition.x]
+            player.currentPosition = newPosition
+            currentRoom = newRoom
+            "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+        } catch (e: Exception) {
+            "Invalid direction: $directionInput."
+        }
+
     fun play() {
         while (true) {
             println(currentRoom.description())
@@ -20,6 +41,7 @@ object Game {
             printPlayerStatus(player)
             print("> Enter your command: ")
             println(GameInput(readLine()).processCommand())
+
         }
     }
 
@@ -34,9 +56,24 @@ object Game {
         val command = input.split(" ")[0]
         val argument = input.split(" ").getOrElse(1, { "" })
         fun processCommand() = when(command.toLowerCase()){
+            "move" -> move(argument);
+            "quit" -> exitGame();
+            "exit" -> exitGame();
+            "ring" -> bellRing()
             else -> commandNotFound()
         }
         private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+    }
+    private fun exitGame() {
+        println("Thank you for playing NyetHack!")
+        exitProcess(0)
+    }
+    private fun bellRing(){
+        if(currentRoom is TownSquare){
+            (currentRoom as TownSquare).ringBell()
+        } else {
+            println("You are not at the Town Square, friend!")
+        }
     }
 }
 
--- src/main/kotlin/com/bignerdranch/nyethack/Room.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -11,5 +11,5 @@ class TownSquare : Room("Town Square"){
     override val dangerLevel = super.dangerLevel - 3
     private var bellSound = "GWONG"
     final override fun load() = "The villagers rally and cheer as you enter! \n${ringBell()}"
-    private fun ringBell() = "The bell tower announces your arrival. $bellSound"
+    public fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
